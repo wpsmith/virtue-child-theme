0d0f9fab96a54253902955285b311c81dbca5f2e
Christopher Lee <LeeC@sudbury.ma.us>
2020-06-02 14:31:31 -0400

    Added RSS feed changes.

--- functions.php
+++ functions.php
@@ -13,3 +13,18 @@ function theme_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+/* Modify RSS feeds to use a different unique ID based on the story modification date.  This is used by an RSS to email service so that changes to meetings are treated as new stories. */
+function sudbury_do_feed_rss2($for_comments)
+{
+	if($for_comments)
+	{
+		load_template(ABSPATH . WPINC . "/feed-rss2-comments.php");
+	}
+	else
+	{
+		load_template(get_template_directory() . "/sudbury-feed-rss2.php");
+	}
+}
+remove_all_actions("do_feed_rss2");
+add_action("do_feed_rss2", "sudbury_do_feed_rss2", 10, 1);
